c5264f187d6e2004d6f566b3a34365178c8f4fb3
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-22 14:47:16 +0300

    Initial Commit

---
+++ UnitTestingSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UnitTestingSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 22.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UnitTestingSwiftUI/UnitTestingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingSwiftUIApp.swift
+//  UnitTestingSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
